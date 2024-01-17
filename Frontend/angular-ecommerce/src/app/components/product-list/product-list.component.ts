6489324b42f76024bc089adbbcc5c818e545e4ea
Igor Tudisco <igortudisco@hotmail.com>
2024-01-17 19:35:49 -0300

    A Cart-Item and Cart Service component was created

--- Frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -119,4 +119,10 @@ export class ProductListComponent implements OnInit {
     }
   }
 
+  addToCart(theProduct: Product) {
+
+    console.log(`Adding to cart: ${theProduct.name},  ${theProduct.unitPrice}`);
+
+  }
+
 }
