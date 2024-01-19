deec4cae900db340b784d7e7e2ed94f08f343e22
Igor Tudisco <igortudisco@hotmail.com>
2024-01-19 16:34:46 -0300

    It updated the component Details to add a new product at Cart Item

--- Frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ Frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -15,6 +17,7 @@ export class ProductDetailsComponent implements OnInit {
   constructor(
 
     private productService: ProductService,
+    private cartService: CartService,
     private router: ActivatedRoute
 
   ) { }
@@ -38,6 +41,15 @@ export class ProductDetailsComponent implements OnInit {
     );
 
     console.log(this.product);
+
+  }
+
+  addToCart() {
+
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+
+    this.cartService.addToCart(theCartItem);
 
   }
 
